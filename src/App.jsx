c119b52a2d8020762898d23aca6a2f8616722ba1
HJ <shany34@naver.com>
2024-11-27 23:52:37 +0900

    tailwind 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./assets/scss/style.scss";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header className="header w-full">
+        <h1>ArtMoments</h1>
+      </header>
+      <div className="contents">
+        <div className="search w-full flex align-middle">
+          <input type="text" />
+          <button></button>
+        </div>
+        <div className="btn-wrap flex justify-items-end">
+          <button className="btn">기록하기</button>
+        </div>
+        <div className="art-list">
+          <ul className="flex gap-4">
+            <li className="rounded-lg">
+              <div className="art-img">
+                <img src="" alt="" />
+              </div>
+              <div className="art-info">
+                <ul>
+                  <li>
+                    <h2>분류</h2>
+                    <p></p>
+                  </li>
+                  <li>
+                    <h2>제목</h2>
+                    <p></p>
+                  </li>
+                  <li>
+                    <h2>날짜</h2>
+                    <p></p>
+                  </li>
+                  <li>
+                    <h2>별점</h2>
+                    <ul className="rate">
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer className="footer"></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
